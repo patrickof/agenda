64f81a0f27eba58ba04c2598b64893a5426f9ce2
Patrick Feitosa <patrickofboy@hotmail.com>
2021-05-14 16:33:25 -0300

    Criação da pagina de listagem de enventos

--- core/views.py
+++ core/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from core.models import Evento
+
+# def index(request):
+#     return redirect('/agenda')
+
 
 # Create your views here.
+def lista_eventos(request):
+    
+    # usuario = request.user
+    # eventos = Evento.objects.filter(usuario=usuario)
+    eventos = Evento.objects.all()
+    dados = {'eventos': eventos}
+    
+    return render(request,'agenda.html', dados)
